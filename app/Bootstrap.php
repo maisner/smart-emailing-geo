a0386da6eb4527487f7c26849dfcfa780c01e527
Michal Šmeidler <smeidler.michal@gmail.com>
2020-04-08 13:24:58 +0200

    Set ENV variables for app

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -7,20 +7,27 @@ use Nette\Configurator;
 
 class Bootstrap {
 	public static function boot(): Configurator {
+		if (!isset($_ENV['APP_INSTANCE_NAME'])) {
+			throw new \InvalidArgumentException('Must by set ENV variable "APP_INSTANCE_NAME"');
+		}
+
+		$logDir = \sprintf('%s/../log/%s', __DIR__, $_ENV['APP_INSTANCE_NAME']);
+		$tempDir = \sprintf('%s/../temp/%s', __DIR__, $_ENV['APP_INSTANCE_NAME']);
+
 		$configurator = new Configurator;
 
-		$configurator->setDebugMode(TRUE);
-
-		$configurator->enableTracy(__DIR__ . '/../log');
-
+		$configurator->setDebugMode($_ENV['DEBUG'] === '1');
 		$configurator->setTimeZone('Europe/Prague');
-		$configurator->setTempDirectory(__DIR__ . '/../temp');
+
+		$configurator->enableTracy($logDir);
+		$configurator->setTempDirectory($tempDir);
 
 		$configurator->createRobotLoader()
 			->addDirectory(__DIR__)
 			->register();
 
 		$configurator->addConfig(__DIR__ . '/config/config.neon');
+		$configurator->addConfig(__DIR__ . '/config/config.php');
 		$configurator->addConfig(__DIR__ . '/config/local.neon');
 
 		return $configurator;
